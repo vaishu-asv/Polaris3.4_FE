ca3e2adeb1cd55eb438bc4d5204f9ff263f51d9a
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-06 19:53:53 +0530

    Initial commit

--- src/app/test/test.module.ts
+++ src/app/test/test.module.ts
@@ -5,12 +5,14 @@ import { CommonModule } from '@angular/common';
 import { TestRoutingModule } from './test-routing.module';
 import { BaselineComponent } from './baseline/baseline.component';
 import { CreatewhatifComponent } from './createwhatif/createwhatif.component';
+import { ShareComponent } from './share/share.component';
 
 
 @NgModule({
   declarations: [
     BaselineComponent,
-    CreatewhatifComponent
+    CreatewhatifComponent,
+    ShareComponent
   ],
   imports: [
     CommonModule,
