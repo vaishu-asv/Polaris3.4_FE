00a8e3972f236fbad60d299fd7bd6e872bef893d
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-06 19:53:51 +0530

    Initial commit

--- src/app/test/test-routing.module.ts
+++ src/app/test/test-routing.module.ts
@@ -1,9 +1,12 @@
+import { ShareComponent } from './share/share.component';
 import { CreatewhatifComponent } from './createwhatif/createwhatif.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'createwhatif', component: CreatewhatifComponent },
+
+  { path: 'share', component: ShareComponent },
 ];
 
 @NgModule({
